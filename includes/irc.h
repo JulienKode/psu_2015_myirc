7bc2b184cfa69a0ce030424d055020afe9d45875
Julien Karst <contact@julienkarst.com>
2016-05-16 10:42:25 +0200

    [IRC] first commit

---
+++ includes/irc.h
@@ -0,0 +1,16 @@
+/*
+** irc.h for  in /home/karst_j/PSU_2015_myirc/includes
+**
+** Made by Julien Karst
+** Login   <karst_j@epitech.net>
+**
+** Started on  Mon May 16 10:40:15 2016 Julien Karst
+** Last update Mon May 16 10:41:05 2016 
+*/
+
+#ifndef	IRC_H_
+#define	IRC_H_
+
+
+
+#endif	/* IRC_H_ */
