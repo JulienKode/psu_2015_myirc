e3dda3586b22a821230102b5d3ab44490c4548a3
Julien Karst <contact@julienkarst.com>
2016-05-25 18:46:42 +0200

    [IRC][Circbuff] Change the structure and add a main for test

---
+++ sources/utils/main.c
@@ -0,0 +1,15 @@
+/*
+** main.c for  in /home/karst_j/PSU_2015_myirc/sources/utils
+**
+** Made by Julien Karst
+** Login   <karst_j@epitech.net>
+**
+** Started on  Wed May 25 16:52:24 2016 Julien Karst
+** Last update Wed May 25 16:52:58 2016 
+*/
+
+int main()
+{
+
+
+}
