4d5f5afeeed85aa21bf3af4d9291de5d440805d2
Julien Karst <contact@julienkarst.com>
2016-05-25 20:44:02 +0200

    [IRC][UTILS] Change header

--- sources/utils/circbuff/utils_circbuff.h
+++ sources/utils/circbuff/utils_circbuff.h
@@ -5,18 +5,24 @@
 ** Login   <karst_j@epitech.net>
 **
 ** Started on  Wed May 18 21:26:09 2016 Julien Karst
-** Last update Wed May 25 17:55:04 2016 
+** Last update Wed May 25 20:42:03 2016 
 */
 
 #ifndef		UTILS_CIRCBUFF_H_
 #define		UTILS_CIRCBUFF_H_
 
+#include	<stdlib.h>
+
 typedef struct	s_circbuff
 {
   char		*buffer;
   int		rpos;
-    int		wpos;
+  int		wpos;
   int		maxLen;
 }		t_circbuff;
+
+char		*circbuff_read(t_circbuff*);
+int		circbuff_write(t_circbuff *, char *);
+t_circbuff	circbuff_create(int);
 
 #endif		/* UTILS_CIRCBUFF_H_ */
