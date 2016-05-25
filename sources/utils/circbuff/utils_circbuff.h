40e512f9d36f9fe3ae9752d43b8986cfb0a741de
Julien Karst <contact@julienkarst.com>
2016-05-25 20:46:36 +0200

    [IRC][Circbuff] Header add perror includes

--- sources/utils/circbuff/utils_circbuff.h
+++ sources/utils/circbuff/utils_circbuff.h
@@ -1,17 +1,18 @@
 /*
-** utils_circbuff.h for  in /home/karst_j/PSU_2015_myirc/includes
+1;2802;0c** utils_circbuff.h for  in /home/karst_j/PSU_2015_myirc/includes
 **
 ** Made by Julien Karst
 ** Login   <karst_j@epitech.net>
 **
 ** Started on  Wed May 18 21:26:09 2016 Julien Karst
-** Last update Wed May 25 20:42:03 2016 
+** Last update Wed May 25 20:46:17 2016 
 */
 
 #ifndef		UTILS_CIRCBUFF_H_
 #define		UTILS_CIRCBUFF_H_
 
 #include	<stdlib.h>
+#include	<stdio.h>
 
 typedef struct	s_circbuff
 {
