c56f19478081b66115589da62ac64e4280b46c41
Julien Karst <contact@julienkarst.com>
2016-05-25 21:26:21 +0200

    [IRC][Circbuff] Header

--- includes/utils_circbuff.h
+++ includes/utils_circbuff.h
@@ -1,21 +1,29 @@
 /*
-** utils_circbuff.h for  in /home/karst_j/PSU_2015_myirc/includes
+1;2802;0c** utils_circbuff.h for  in /home/karst_j/PSU_2015_myirc/includes
 **
 ** Made by Julien Karst
 ** Login   <karst_j@epitech.net>
 **
 ** Started on  Wed May 18 21:26:09 2016 Julien Karst
-** Last update Wed May 18 21:29:28 2016 
+** Last update Wed May 25 20:46:17 2016 
 */
 
 #ifndef		UTILS_CIRCBUFF_H_
 #define		UTILS_CIRCBUFF_H_
 
+#include	<stdlib.h>
+#include	<stdio.h>
+
 typedef struct	s_circbuff
 {
   char		*buffer;
-  int		pos;
+  int		rpos;
+  int		wpos;
   int		maxLen;
 }		t_circbuff;
+
+char		*circbuff_read(t_circbuff*);
+int		circbuff_write(t_circbuff *, char *);
+t_circbuff	circbuff_create(int);
 
 #endif		/* UTILS_CIRCBUFF_H_ */
