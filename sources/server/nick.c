ed5070be02f8ba90c6f54c967ebccff34e9107a1
Julien Karst <contact@julienkarst.com>
2016-05-30 18:18:19 +0200

    [IRC] Nick

---
+++ sources/server/nick.c
@@ -0,0 +1,36 @@
+/*
+** nick.c for  in /home/karst_j/PSU_2015_myirc/sources/server
+**
+** Made by
+** Login   <karst_j@epitech.net>
+**
+** Started on  Mon May 30 18:16:50 2016
+** Last update Mon May 30 18:17:55 2016 
+*/
+
+#include	"irc.h"
+
+void		cmd_nick
+(int fd, t_channel *chan,
+ fd_set *fd_write, char *nick)
+{
+  char		*msg;
+
+  (void) fd_write;
+  if (nick == NULL)
+    dprintf(fd, "431 * NICK :No nickname given\r\n");
+  else if (nick_exists(chan, nick))
+    dprintf(fd, "433 * %s :Nickname is already in use\r\n", nick);
+  else
+    {
+      msg = malloc(7 + strlen(chan->nick[fd]) + strlen(nick));
+      if (msg == NULL)
+	exit(42);
+      msg = strcpy(msg, chan->nick[fd]);
+      msg = strcat(msg, " NICK ");
+      msg = strcat(msg, nick);
+      global_message(chan, msg);
+      nick[strlen(nick) - 1] = 0;
+      chan->nick[fd] = strdup(nick);
+    }
+}
