e3df724d8d026da65e174531edfee7c0db2c31e0
Julien Karst <contact@julienkarst.com>
2016-05-30 22:03:25 +0200

    [IRC][USERS]

---
+++ sources/server/users.c
@@ -0,0 +1,45 @@
+/*
+** users.c for  in /home/karst_j/PSU_2015_myirc/sources/server
+**
+** Made by
+** Login   <karst_j@epitech.net>
+**
+** Started on  Mon May 30 21:40:00 2016
+** Last update Mon May 30 22:02:14 2016 
+*/
+
+#include	"irc.h"
+
+int		nb_of_users(t_channel *chan, int fd)
+{
+  t_channel	*tmp;
+  int		i;
+
+  i = 0;
+  tmp = chan;
+  while (tmp->root == 0)
+    tmp = tmp->next;
+  tmp = tmp->next;
+  while (tmp->root == 0)
+    {
+      if (tmp && tmp->fd_type[fd] != FD_FREE)
+	i++;
+      tmp = tmp->next;
+    }
+  return (i);
+}
+
+void		cmd_users
+(int fd, t_channel *chan,
+ fd_set *fd_write, char *arg_one)
+{
+  int		res;
+
+  (void) fd_write;
+  (void) arg_one;
+  res = nb_of_users(chan, fd);
+  dprintf(fd,
+	  "265 to :Current local users: %d  Max: %d\r\n"
+	  "266 to :Current global users: %d  Max: %d\r\n",
+	  res, res, res, res );
+}
