8caf2a9e63a2c66de812d897fd03a7f8b9d2a7b9
Julien Karst <contact@julienkarst.com>
2016-06-03 00:35:48 +0200

    [IRC][Names]

--- sources/server/names.c
+++ sources/server/names.c
@@ -5,16 +5,35 @@
 ** Login   <karst_j@epitech.net>
 **
 ** Started on  Wed Jun  1 22:20:54 2016  Julien Karst
-** Last update Wed Jun  1 23:19:08 2016 
+** Last update Fri Jun  3 00:35:17 2016 
 */
 
 #include	"irc.h"
 
 static void	print_all_channel(t_channel *chan, int fd)
 {
+  int		c;
+  t_channel     *tmp;
 
-
-  dprintf(fd, "366 %s * :End of /NAMES list.\r\n", chan->nick[fd]);
+  tmp = chan;
+  c = -1;
+  while (tmp->root == 0)
+    tmp = tmp->next;
+  tmp = tmp->next;
+  while (tmp->root == 0)
+    {
+      if (tmp->name && tmp->fd_type[fd] != FD_FREE)
+	{
+	  dprintf(fd, ":irc.localhost 353 %s = %s :%s",
+		  chan->nick[fd], chan_name, chan->nick[fd]);
+	  while (++c < MAX_FD)
+	    if (tmp->fd_type[c] != FD_FREE && c != tmp->creator && c != fd)
+	      dprintf(fd, " %s", tmp->nick[c]);
+	  if (tmp->fd_type[tmp->creator] != FD_FREE)
+	    dprintf(fd, " @%s",  tmp->nick[tmp->creator]);
+	}
+      tmp = tmp->next;
+    }
 }
 
 static void	print_channel(t_channel *chan, int fd, char *str)
@@ -22,8 +41,13 @@ static void	print_channel(t_channel *chan, int fd, char *str)
   t_channel	*tmp;
 
   tmp = found_channel_by_name(chan, str);
-  dprintf(fd, "366 %s %s :End of /NAMES list.\r\n", chan->nick[fd], str);
-  dprintf(fd, "366 %s %s :End of /NAMES list.\r\n", chan->nick[fd], str);
+  dprintf(fd, ":irc.localhost 353 %s = %s :%s",
+	  chan->nick[fd], chan_name, chan->nick[fd]);
+  while (++c < MAX_FD)
+    if (tmp->fd_type[c] != FD_FREE && c != tmp->creator && c != fd)
+      dprintf(fd, " %s", tmp->nick[c]);
+  if (tmp->fd_type[tmp->creator] != FD_FREE)
+    dprintf(fd, " @%s",  tmp->nick[tmp->creator]);
 }
 
 void		cmd_names
@@ -36,4 +60,6 @@ void		cmd_names
     print_all_channel(chan, fd);
   else
     print_channel(chan, fd, arg_one);
+  dprintf(fd, "\r\n:irc.localhost 366 %s * "
+	  ":End of /NAMES list.\r\n", chan->nick[fd]);
 }
