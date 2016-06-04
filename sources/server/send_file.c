015b657c49bf893514755933e6983b64749fed31
Julien Karst <contact@julienkarst.com>
2016-06-04 17:08:39 +0200

    [IRC][FILE]

---
+++ sources/server/send_file.c
@@ -0,0 +1,18 @@
+/*
+** send_file.c for  in /home/karst_j/PSU_2015_myirc
+**
+** Made by
+** Login   <karst_j@epitech.net>
+**
+** Started on  Sat Jun  4 17:04:36 2016
+** Last update Sat Jun  4 17:05:42 2016 
+*/
+
+#include "irc.h"
+
+void    cmd_send(int fd, t_channel *chan, char *arg_one)
+{
+  (void) fd;
+  (void) chan;
+  (void) arg_one;
+}
