48f287aeffcce4f2dbbc23ab3c7faeaa67decc8d
Julien Karst <contact@julienkarst.com>
2016-06-05 23:17:09 +0200

    [IRC][Send] Buff

--- sources/client/send_buff.cpp
+++ sources/client/send_buff.cpp
@@ -5,7 +5,7 @@
 ** Login   <karst_j@epitech.net>
 **
 ** Started on  Sat Jun  4 16:34:17 2016
-** Last update Sun Jun  5 21:13:49 2016
+** Last update Sun Jun 05 23:14:49 2016 Julien Karst
 */
 
 #include	"../../includes/irc.h"
@@ -37,6 +37,22 @@ static int	check_conform(char *str)
   return (0);
 }
 
+static int  isPrintable(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (*str)
+    {
+        if (*str >= 32 && *str <= 120)
+            return (i);
+        else
+            i++;
+        str++;
+    }
+    return (i);
+}
+
 char		*get_buff_read_underground(t_client *client)
 {
   int		save;
@@ -49,15 +65,22 @@ char		*get_buff_read_underground(t_client *client)
   if (str)
     {
       tmp = strtok(strdup(str), "\n");
+      if (isPrintable(tmp) != 0)
+      {
+          printf("TEST %s\n", tmp);
+  	  client->circbuff_read.rpos = save + isPrintable(tmp);
+      client->circbuff_r = 1;
+  	  return (NULL);
+      }
       if (strlen(tmp) == strlen(str))
 	{
 	  client->circbuff_r = 0;
 	  return (NULL);
 	}
       if (check_conform(tmp) == 0)
-	{
-	  client->circbuff_read.rpos = save;
-	  return (NULL);
+      {
+  	  client->circbuff_read.rpos = save;
+  	  return (NULL);
 	}
       if (strlen(tmp) != strlen(str))
         {
